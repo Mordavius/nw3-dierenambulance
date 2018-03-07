8a60c5af60f015ea6f8d01065fb23ffd2a1ee203
Laurens-Post <contact@lphosting.nl>
2018-03-07 22:52:22 +0100

    Edits for bootstrap & styles

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dierenambulance | Dashboard</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 4.0.0 -->
+    <link rel="stylesheet" href="{{ url('/') }}/css/app.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ url('/') }}/css/style.css">
+
+
+</head>
+<body>
+
+@yield('content')
+
+<!-- Bootstrap 4.0.0 -->
+<script src="{{ url('/') }}/js/app.js"></script>
+
+</body>
+</html>
