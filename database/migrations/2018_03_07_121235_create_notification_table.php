93e7eb0cafc3971d396c7772abe591ad2ea46687
Laurens-Post <contact@lphosting.nl>
2018-03-07 13:29:01 +0100

    Table + Controller for Notification

---
+++ database/migrations/2018_03_07_121235_create_notification_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('date');
+            $table->time('time');
+            $table->string('address');
+            $table->integer('housenumber');
+            $table->string('postalcode');
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification');
+    }
+}
