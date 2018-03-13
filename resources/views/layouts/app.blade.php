416d89f04d0243f84c9640e78942d85535edfc88
Geart Otten <geartslibbie@gmail.com>
2018-03-13 14:43:20 +0100

    Changed app so users can go to profile from everywhere

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,19 +35,23 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+
                         @else
+                        <li><a class="nav-link" href="{{ route('register') }}">Registreer gebruikers</a></li>
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="profiel">Profiel</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        Logout
+                                        Uitloggen
                                     </a>
+
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
