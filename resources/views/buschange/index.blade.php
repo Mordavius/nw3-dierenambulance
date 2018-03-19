25be555ff6e0f1804727c0060be4957f56675c5d
Laurens-Post <contact@lphosting.nl>
2018-03-19 15:08:18 +0100

    fix

---
+++ resources/views/buschange/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <nav class="navbar navbar-light bg-light">
+      </nav>
+      <div class="card">
+        <div class="card-header">Buswissel</div>
+        <div class="card-body">
+          <div class="box-body">
+            <a href="{{ route('buswissel.create') }}" class="btn btn-primary">Nieuwe melding aanmaken</a><br /><br />
+
+            @if (session('status'))
+              <div class="alert alert-success">
+                {{ session('status') }}
+              </div>
+            @endif
+          @if (! $buschanges->count())
+            <div class="alert alert-danger">
+              <strong>Geen buswissels gevonden</strong>
+            </div>
+          @else
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <td>Bus</td>
+                  <td>Datum</td>
+                  <td>Van</td>
+                  <td>Naar</td>
+                  <td>Kilometer stand</td>
+                </tr>
+              </thead>
+
+              <tbody>
+                @foreach($buschanges as $buschange)
+                  <tr>
+                    <td>{{$buschange->bus}}</td>
+                    <td>{{$buschange->date}}</td>
+                    <td>{{$buschange->from}}</td>
+                    <td>{{$buschange->to}}</td>
+                    <td>{{$buschange->kilometerstraveled}}</td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+            @endif
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 
@@ -23,7 +23,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/meldingen', 'HomeController@ambulance')->name('meldingen');
 
-Route::get('register', 'HomeController@register')->name('register');
+Route::get('/register', 'HomeController@register')->name('register')->middleware('rolecheck');
 
 Route::get('ambulance', 'HomeController@ambulance')->name('ambulance')->middleware('rolecheck');
 
@@ -37,5 +37,6 @@ Route::get('search', [
 // CRUD Notification Controller
 Route::resource('melding','NotificationController');
 Route::resource('profiel','ProfileController');
+Route::resource('buswissel', 'BusChangeController');
 
 //Route::resource('profiel', 'ProfileController');
