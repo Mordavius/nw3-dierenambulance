f5c42e7d6d9f706e6ff47e1a38cb7bca4cdcdd03
Laurens-Post <contact@lphosting.nl>
2018-05-08 15:07:01 +0200

    Login fixes + authentication checks on pages

--- routes/web.php
+++ routes/web.php
@@ -11,51 +11,51 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', ['middleware' => 'guest', function() {
     return view('auth/login');
-});
+}]);
 
 
 
 Auth::routes();
 
-Route::get('home', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index')->name('home')->middleware('auth');
 
-Route::get('meldingen', 'HomeController@ambulance')->name('meldingen');
+Route::get('meldingen', 'HomeController@ambulance')->name('meldingen')->middleware('auth');
 
-Route::get('ambulance', 'HomeController@ambulance')->name('ambulance')->middleware('rolecheck');
+Route::get('ambulance', 'HomeController@ambulance')->name('ambulance')->middleware('rolecheck', 'auth');
 
 
-Route::get('/register', 'HomeController@register')->name('register')->middleware('rolecheck');
+Route::get('/register', 'HomeController@register')->name('register')->middleware('rolecheck', 'auth');
 
 Route::get('search', [
     'uses' => 'TicketController@index',
     'as' => 'search',
 ]);
 
-Route::get('/administratie', 'AdministrationController@index')->name('Administratie');
-Route::get('/exporteren', 'AdministrationController@export')->name('Exporteren');
-Route::get('downloadExcel', 'AdministrationController@downloadExcel');
+Route::get('/administratie', 'AdministrationController@index')->name('Administratie')->middleware('auth');
+Route::get('/exporteren', 'AdministrationController@export')->name('Exporteren')->middleware('auth');
+Route::get('downloadExcel', 'AdministrationController@downloadExcel')->middleware('auth');
 // Route::get('pdfview',array('as'=>'pdfview','uses'=>'AdministrationController@pdfview'));
 
 // CRUD Notification Controllers
-Route::resource('melding', 'TicketController')->middleware('rolecheck');
+Route::resource('melding', 'TicketController')->middleware('rolecheck', 'auth');
 
 //CRUD Bus Controllers
-Route::resource('bus', 'BusController')->middleware('rolecheck');
+Route::resource('bus', 'BusController')->middleware('rolecheck', 'auth');
 
 //CRUD Known addresses Controllers
-Route::resource('bekende-adressen', 'KnownController')->middleware('rolecheck');
+Route::resource('bekende-adressen', 'KnownController')->middleware('rolecheck', 'auth');
 
 
 // CRUD Profile Controllers
-Route::resource('profiel', 'ProfileController');
+Route::resource('profiel', 'ProfileController')->middleware('auth');
 
 // CRUD User Controllers
-Route::resource('leden', 'UserController')->middleware('rolecheck');
+Route::resource('leden', 'UserController')->middleware('rolecheck', 'auth');
 
-Route::resource('buswissel', 'BusChangeController');
+Route::resource('buswissel', 'BusChangeController')->middleware('auth');
 
 
 
-Route::get('/location/{id}', 'LocationController@setLocation');
+Route::get('/location/{id}', 'LocationController@setLocation')->middleware('auth');
