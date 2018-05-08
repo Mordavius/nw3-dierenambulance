0da1c8cf5c1d599a7ebbd52440d40cc5572e9a25
Laurens-Post <contact@lphosting.nl>
2018-05-08 11:44:14 +0200

    Added comments for the models

--- app/Finance.php
+++ app/Finance.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Finance extends Model
 {
+    // Set the id for the finance table
     protected $primaryKey = 'finance_id';
-    //
+
 }
--- app/Ticket.php
+++ app/Ticket.php
@@ -7,19 +7,23 @@ use Carbon\Carbon;
 
 class Ticket extends Model
 {
+    // Set the columns for storing data in the database
     protected $fillable = ['destination_id', 'bus_id' , 'animal_id', 'finance_id', 'date','time', 'centralist',
     'reporter_name', 'telephone', 'driver', 'passenger'];
 
+    // Tickets could have many destinations
     public function destination()
     {
         return $this->hasMany('App\Destination');
     }
 
+    // Tickets could only have one animal
     public function animal()
     {
         return $this->hasOne('App\Animal');
     }
 
+    // Tickets could only have one bus
     public function bus()
     {
         return $this->hasOne('App\Bus');
