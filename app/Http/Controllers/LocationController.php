ba9630adc2b5975fd9dcd5f548ba6a93b21b3626
Mordavius <g.w.n.h.iskondos@gmail.com>
2018-05-22 21:28:31 +0200

    added sms gateway to laravel and ability to send sms with location link

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Location;
+use Bjrnblm\Messagebird\Messagebird;
+use MessageBird\Client;
+use MessageBird\Common\HttpClient;
+use MessageBird\Resources\Chat\Message;
+use MessageBird\Resources\Chat\Platform;
+use MessageBird\Common;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
 
 class LocationController extends Controller
@@ -43,6 +50,35 @@ class LocationController extends Controller
         }
         //return response()->json(array('msg' => $location), 200);
     }
+
+    public function askLocationMail(Request $request)
+    {
+        $data = array('name'=>"Virat Gandhi", 'link' => $request->id);
+        Mail::send('mail', $data, function($message) {
+            $message->to('abc@gmail.com', 'Tutorials Point')->subject
+            ('Laravel HTML Testing Mail');
+            $message->from('xyz@gmail.com','Virat Gandhi');
+        });
+        return response('iets',200);
+    }
+
+    public function askLocationSMS(Request $request)
+    {
+        $client = new Client('acceskey');
+        $messagebird = new Messagebird($client);
+        $message = $messagebird->createMessage("Dierenambu",["+31"], " mainlink/location/".$request->id);
+
+        if (is_object($message) && $message->recipients->items[0]->status === 'sent') {
+            return response(json_encode('message sent'), 200);
+        }
+
+        if (in_array($message, $messagebird->getErrorMessages())){
+            // TODO: sentry log (vaste error)
+        }
+        // TODO: sentry error (API error)
+        return response(json_encode('something went wrong'), 400);
+    }
+
     public function markersOnCoordinates()
     {
         //
--- public/js/table-directive.js
+++ public/js/table-directive.js
@@ -165,14 +165,28 @@ var app = angular.module("app", [])
         });
     }
     $('#sendLocationButton').click(sendLocationRequest);
-    function sendLocationRequest(){
+    // function sendLocationRequest(){
+    //     $.ajax({
+    //         type:'POST',
+    //         url:'/api/mail',
+    //         data: {id: locationId},
+    //         success: function() {
+    //             alert("mail verzonden");
+    //             getLocationRecord();
+    //         }
+    //     });
+    // }
+    function sendLocationRequest() {
         $.ajax({
-            type:'POST',
-            url:'/api/mail',
+            type: 'POST',
+            url: '/api/sms',
             data: {id: locationId},
-            success: function() {
-                alert("mail verzonden");
+            success: function () {
+                alert("SMS verzonden");
                 getLocationRecord();
+            },
+            error: function () {
+                alert("Er is iets fout gegaan, als u dit bericht vaker ziet neem dan contact op met de beheerder");
             }
         });
     }
