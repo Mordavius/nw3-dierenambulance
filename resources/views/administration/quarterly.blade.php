494211a7c500f78a5a4ea7ce73a29ed6f6d184ff
Mordavius <g.w.n.h.iskondos@gmail.com>
2018-05-22 15:10:06 +0200

    added link to export overview

--- resources/views/administration/quarterly.blade.php
+++ resources/views/administration/quarterly.blade.php
@@ -3,37 +3,33 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Kwartaaloverzicht
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <td width="80">Naam</td>
+                                <td width="80">Jaar</td>
+                                <td width="80">Downloaden</td>
+                            </tr>
+                            </thead>
+                            <tbody>
             @foreach($quarterlies as $quarterlies)
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            Profiel
-                        </div>
-                        <div class="card-body">
-                            @if(session('success'))
-                                <div class="alert alert-info">
-                                    {{ session('success') }}
-                                </div>
-                            @endif
-                            <table class="table table-bordered">
-                                <thead>
-                                <tr>
-                                    <td width="80">Naam</td>
-                                    <td width="80">Jaar</td>
-                                    <td width="80">Downloaden</td>
-                                </tr>
-                                </thead>
-                                <tbody>
                                 <tr>
                                     <td>{{ $quarterlies->name }}</td>
                                     <td>{{ $quarterlies->year }}</td>
                                     <td><a href="download/{{ $quarterlies->name }}.xlsx">downloaden</a></td>
                                 </tr>
+                            @endforeach
                             </table>
                         </div>
                     </div>
                 </div>
-            @endforeach
+
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::group(['middleware' => 'IsAdmin'], function () {
     Route::get('/register', 'HomeController@register')->name('register')->middleware('auth');
     Route::get('/administratie', 'AdministrationController@index')->name('Administratie')->middleware('auth');
     Route::get('/exporteren', 'AdministrationController@export')->name('Exporteren')->middleware('auth');
-    Route::get('/administratie/kwartaalverslagen', 'AdministrationController@quartexports')->name('Kwartaaloverzicht')->middleware('auth');
+    Route::get('/kwartaaloverzicht', 'AdministrationController@quartexports')->name('Kwartaaloverzicht')->middleware('auth');
     Route::get('/administratie/download/{filename}', 'AdministrationController@quartdownload')->middleware('auth');
     Route::get('downloadExcel', 'AdministrationController@downloadExcel')->middleware('auth');
 
