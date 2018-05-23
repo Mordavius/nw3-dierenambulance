0c7c6ec5ef5f7b888cae00d1dd7d7e6ca754b1a8
Mordavius <g.w.n.h.iskondos@gmail.com>
2018-05-23 13:22:34 +0200

    added create_knowns_table migration and changed buschange view, also removed a fillable in the Known model

---
+++ database/migrations/2018_05_23_124902_create_knowns_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKnownsTable extends Migration
+{
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+    {
+                Schema::create('knowns', function (Blueprint $table) {
+                        $table->increments('id');
+                        $table->string('location_name');
+                        $table->string('postal_code');
+                        $table->string('address');
+                        $table->string('house_number');
+                        $table->string('city');
+                        $table->timestamps();
+                    });
+            }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+                Schema::dropIfExists('knowns');
+            }
+}
