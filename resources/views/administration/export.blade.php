5875540ecf5406530f73dd844521b1b3c4545cc3
Tim de Raaf <timraaf@hotmail.com>
2018-06-08 10:59:54 +0200

    Export

--- resources/views/administration/export.blade.php
+++ resources/views/administration/export.blade.php
@@ -14,6 +14,17 @@
     {{Form::open(array('url' => 'downloadExcel'))}}
     <input type="date" name="startdate" id="startdate"/>
     <input type="date" name="enddate" id="enddate"/>
+    <select placeholder="Dier" id="animal" type="text" name="animal">
+      <option value="">Hoi</option>
+      <option value="">Hoi</option>
+      <option value="">Hoi</option>
+    </select>
+    <select placeholder="Gemeente" id="township" type="text" name="township">
+      <option value="">Hoi</option>
+      <option value="">Hoi</option>
+      <option value="">Hoi</option>
+    </select>
+    <input type="checkbox" id="financien" name="Met financiën" value="Met financiën"><label for="financien">Met financiën</label>
     <button class="btn btn-success full-width">
         Exporteren naar Excel
     </button>
