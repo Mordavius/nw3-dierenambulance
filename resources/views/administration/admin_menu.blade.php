2411833606a3a054979f2a8cca5c464bbc16ad2a
Geart Otten <geartslibbie@gmail.com>
2018-06-20 14:25:51 +0200

    Fixed bug with caps

--- resources/views/administration/admin_menu.blade.php
+++ resources/views/administration/admin_menu.blade.php
@@ -3,26 +3,26 @@
     <li class="menu-item {{ (Request::is('administratie') ? 'active' : '') }}">
         <a href="/administratie">
             <div class="svgwrapper icon">
-            <?php echo file_get_contents("images/users.svg"); ?> <span class="icon"> Gebruikers </span>
+            <?php echo file_get_contents("images/Users.svg"); ?> <span class="icon"> Gebruikers </span>
             </div>
         </a>
     </li>
     <li class="menu-item {{ (Request::is('bus') ? 'active' : '') }}">
       <a href="/bus">
           <div class="svgwrapper icon">
-          <?php echo file_get_contents("images/car.svg"); ?> <span class="icon"> Voertuigen </span>
+          <?php echo file_get_contents("images/Car.svg"); ?> <span class="icon"> Voertuigen </span>
           </div>      </a>
     </li>
     <li class="menu-item {{ (Request::is('bekende-adressen') ? 'active' : '') }}">
       <a href="/bekende-adressen">
           <div class="svgwrapper icon">
-          <?php echo file_get_contents("images/location.svg"); ?> <span class="icon"> Bekende adressen </span>
+          <?php echo file_get_contents("images/Location.svg"); ?> <span class="icon"> Bekende adressen </span>
           </div>      </a>
     </li>
     <li class="menu-item {{ (Request::is('exporteren') ? 'active' : '') }}">
       <a href="/exporteren">
           <div class="svgwrapper icon">
-          <?php echo file_get_contents("images/export.svg"); ?> <span class="icon"> Exporteren </span>
+          <?php echo file_get_contents("images/Export.svg"); ?> <span class="icon"> Exporteren </span>
           </div>      </a>
     </li>
   </ul>
