fa7df777de6c8c32ee218bd52215fe8234087ac9
Geart Otten <geartslibbie@gmail.com>
2018-06-20 14:37:09 +0200

    Names changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
             <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
                 <div class="container">
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        <img class="logo" src="{{ asset('images/dierenambulance-logo.png') }}">
+                        <img class="logo" src="{{ asset('images/Dierenambulance-logo.png') }}">
                     </a>
 
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
