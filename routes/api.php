852495205177380b59a7ec046309ee7f72e2e4ee
vdlindenmark <vdlindenmark@gmail.com>
2018-06-26 11:40:27 +0200

    Removed unused askLocationMail()

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::post('/mail', 'LocationController@askLocationMail');
 Route::post('/sms', 'LocationController@askLocationSMS');
 Route::get('/location/show/{id}', 'LocationController@getLocation');
 Route::get('/ticketfilter/{date}/{animal}/{city}', 'TicketController@filterTickets');
 Route::post('/location/write', 'LocationController@writeLocation');
 Route::get('/location/{id}', 'LocationController@setLocation');
-// Route::get('/coordinates/show/{active}', 'LocationController@markersOnCoordinates')
