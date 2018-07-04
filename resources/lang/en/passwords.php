3eeb3fcababce5050b02bd237f1cf436831cd1f5
jurw <jurwiersema@hotmail.nl>
2018-07-04 11:49:55 +0200

    Styled wachtwoord vergeten page

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -15,8 +15,8 @@ return [
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'sent' => 'We hebben uw link voor het opnieuw instellen van uw wachtwoord per e-mail verzonden!',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "We kunnen geen gebruiker met dat e-mailadres vinden.",
 
 ];
