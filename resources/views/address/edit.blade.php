1976b2cb77abe5a2fa1805d86c83c0d405631a57
Mordavius <g.w.n.h.iskondos@gmail.com>
2018-07-05 14:15:49 +0200

    made editing of known addresses possible

---
+++ resources/views/address/edit.blade.php
@@ -0,0 +1,72 @@
+@extends(Auth::user()->isAdmin() ? 'layouts.app' : 'layouts.appambulance');
+
+@section('content')
+    @include('administration.admin_menu')
+    @include('administration.edit_menu')
+@section('body_class', 'edit_page')
+<div class="wrapper">
+    <section class="content">
+        <h1>Adres wijzigen</h1>
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        {!! Form::model($known, [
+            'method' => 'PUT',
+            'route' => ['bekende-adressen.update', 'id' => $known->id]])
+        !!}
+        <div class="form-group{{ $errors->has('location_name') ? 'has-error' : ''}}">
+            {!! Form::text('location_name', null, ['placeholder' => 'Naam'], ['class' => 'form-control']) !!}
+
+            @if($errors->has('location_name'))
+                <br /><span class="alert-danger">{{ $errors->first('location_name') }}</span><br /><br />
+            @endif
+        </div>
+
+        <div class="form-group{{ $errors->has('postal_code') ? 'has-error' : ''}}">
+            {!! Form::text('postal_code', null, ['placeholder' => 'Postcode'], ['class' => 'form-control']) !!}
+
+            @if($errors->has('postal_code'))
+                <br /><span class="alert-danger">{{ $errors->first('postal_code') }}</span><br /><br />
+            @endif
+        </div>
+
+        <div class="form-group{{ $errors->has('address') ? 'has-error' : ''}}">
+            {!! Form::text('address', null,['placeholder' => 'Adres'], ['class' => 'form-control']) !!}
+
+            @if($errors->has('address'))
+                <br /><span class="alert-danger">{{ $errors->first('address') }}</span><br /><br />
+            @endif
+        </div>
+
+        <div class="form-group{{ $errors->has('house_number') ? 'has-error' : ''}}">
+            {!! Form::text('house_number', null, ['placeholder' => 'Nummer'], ['class' => 'form-control']) !!}
+
+            @if($errors->has('house_number'))
+                <br /><span class="alert-danger">{{ $errors->first('house_number') }}</span><br /><br />
+            @endif
+        </div>
+
+        <div class="form-group{{ $errors->has('city') ? 'has-error' : ''}}">
+            {!! Form::text('city', null, ['placeholder' => 'Plaats'], ['class' => 'form-control']) !!}
+
+            @if($errors->has('city'))
+                <br /><span class="alert-danger">{{ $errors->first('city') }}</span><br /><br />
+            @endif
+        </div>
+
+        <div class="form-group{{ $errors->has('township') ? 'has-error' : ''}}">
+            {!! Form::text('township', null, ['placeholder' => 'Gemeente'], ['class' => 'form-control']) !!}
+        </div>
+
+        @if($errors->has('township'))
+            <br /><span class="alert-danger">{{ $errors->first('township') }}</span><br /><br />
+        @endif
+
+        {!! Form::submit('Wijzigen', ['class' => 'btn btn-success']) !!}
+        {!! Form::close()   !!}
+    </section>
+</div>
+@endsection
--- resources/views/address/index.blade.php
+++ resources/views/address/index.blade.php
@@ -1,47 +1,55 @@
 @extends(Auth::user()->isAdmin() ? 'layouts.app' : 'layouts.appambulance');
 @section('content')
-@include('administration.admin_menu')
+    @include('administration.admin_menu')
     <div class="wrapper">
-      @if (session('status'))
-          <div class="alert alert-success">
-              {{ session('status') }}
-          </div>
-      @endif
-      <section class="content">
-        <br>
-        <a href="/bekende-adressen/create" class="btn btn-success">
-            Nieuw adres toevoegen
-        </a>
-        <br />
-        <br />
-        <br />
-          @if (! $known->count())
-          <div class="alert alert-danger">
-              <strong>Geen bekende adressen gevonden</strong>
-          </div>
-          @else
-            <div class="list">
-              @foreach($known as $known)
-                <div class="list-item">
-                  <!-- <a href=" route('address.edit', $address->id) "> -->
-                    <div class="headings">
-                      <h2>{{ $known->location_name }}</h2>
-                      <span class="subheading">{{ $known->address }} {{$known->house_number}}, {{ $known->postal_code }} {{ $known->city}}</span>
-                    </div>
-                  <!-- </a> -->
-                  <div class="delete">
-                    {!! Form::open(['method' => 'DELETE',
-                    'route' => ['bekende-adressen.destroy', $known->id],
-                    'onsubmit' => 'return confirm("Klik op OK om de melding te verwijderen!")']) !!}
-                        <button type="submit" class="btn-delete">
-                            <img class="icon" src="{{asset('images/delete.svg')}}" alt="Verwijderen"> </li>
-                        </button>
-                    {!! Form::close() !!}
-                  </div>
-                </div>
-              @endforeach
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
             </div>
-          @endif
-      </section>
-</div>
+        @endif
+            @if(session()->has('success'))
+                <div class="alert alert-success">
+                    {{ session()->get('success') }}
+                </div>
+            @endif
+        <section class="content">
+            <br>
+            <a href="/bekende-adressen/create" class="btn btn-success">
+                Nieuw adres toevoegen
+            </a>
+            <br />
+            <br />
+            <br />
+            @if (! $known->count())
+                <div class="alert alert-danger">
+                    <strong>Geen bekende adressen gevonden</strong>
+                </div>
+            @else
+                <div class="list">
+                    @foreach($known as $known)
+                        <div class="list-item">
+                            <a href="{{route('bekende-adressen.edit',$known->id)}}">
+                                <!-- <a href=" route('address.edit', $address->id) "> -->
+                                <div class="headings">
+                                    <h2>{{ $known->location_name }}</h2>
+                                    <span class="subheading">{{ $known->address }} {{$known->house_number}}, {{ $known->postal_code }} {{ $known->city}}</span>
+                                </div>
+                            </a>
+                                <!-- </a> -->
+                                <div class="delete">
+                                    {!! Form::open(['method' => 'DELETE',
+                                    'route' => ['bekende-adressen.destroy', $known->id],
+                                    'onsubmit' => 'return confirm("Klik op OK om de melding te verwijderen!")']) !!}
+                                    <button type="submit" class="btn-delete">
+                                        <img class="icon" src="{{asset('images/delete.svg')}}" alt="Verwijderen"> </li>
+                                    </button>
+                                    {!! Form::close() !!}
+                                </div>
+
+                        </div>
+                    @endforeach
+                </div>
+            @endif
+        </section>
+    </div>
 @endsection
