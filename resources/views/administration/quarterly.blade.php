5157fb8d2a14b98858f8109dadfa1b786e57fbd8
Mordavius <g.w.n.h.iskondos@gmail.com>
2018-07-06 11:08:15 +0200

    changes to quartely.blade

--- resources/views/administration/quarterly.blade.php
+++ resources/views/administration/quarterly.blade.php
@@ -18,7 +18,6 @@
                             </tr>
                             </thead>
                             <tbody>
-
                             @if(!empty($errors->first()))
                                 <div class="row col-lg-12">
                                     <div class="alert alert-danger">
