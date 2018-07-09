29be7cd661708c26cf8a668e5cbf4d86cde5ef78
Mordavius <g.w.n.h.iskondos@gmail.com>
2018-07-09 10:14:14 +0200

    admin link fix

--- resources/views/administration/admin_menu.blade.php
+++ resources/views/administration/admin_menu.blade.php
@@ -2,21 +2,22 @@
   <ul>
     <li class="menu-item {{ (Request::is('administratie') ? 'active' : '') }} {{ (Request::is('register') ? 'active' : '') }}">
         <a href="/administratie">
-
+          <span class="icon"> Gebruikers </span>
         </a>
     </li>
     <li class="menu-item {{ (Request::is('bus') ? 'active' : '') }} {{ (Request::is('bus/*') ? 'active' : '') }}">
       <a href="/bus">
-
+          <span class="icon"> Voertuigen </span>
       </a>
     </li>
     <li class="menu-item {{ (Request::is('bekende-adressen') ? 'active' : '') }} {{ (Request::is('bekende-adressen/*') ? 'active' : '') }}">
       <a href="/bekende-adressen">
-
+          <span class="icon"> Bekende adressen </span>
       </a>
     </li>
     <li class="menu-item {{ (Request::is('exporteren') ? 'active' : '') }}">
       <a href="/exporteren">
+          <span class="icon"> Exporteren </span>
       </a>
     </li>
   </ul>
