fd7e3d9c1b089584fdfe0937097d13ecfdb868af
Mordavius <g.w.n.h.iskondos@gmail.com>
2018-07-09 10:06:32 +0200

    test

--- resources/views/administration/admin_menu.blade.php
+++ resources/views/administration/admin_menu.blade.php
@@ -2,22 +2,22 @@
   <ul>
     <li class="menu-item {{ (Request::is('administratie') ? 'active' : '') }} {{ (Request::is('register') ? 'active' : '') }}">
         <a href="/administratie">
-            <?php echo file_get_contents("/images/Users.svg"); ?> <span class="icon"> Gebruikers </span>
+            {{--<?php echo file_get_contents("images/Users.svg"); ?> <span class="icon"> Gebruikers </span>--}}
         </a>
     </li>
     <li class="menu-item {{ (Request::is('bus') ? 'active' : '') }} {{ (Request::is('bus/*') ? 'active' : '') }}">
       <a href="/bus">
-          <?php echo file_get_contents("/images/Car.svg"); ?> <span class="icon"> Voertuigen </span>
+          {{--<?php echo file_get_contents("images/Car.svg"); ?> <span class="icon"> Voertuigen </span>--}}
       </a>
     </li>
     <li class="menu-item {{ (Request::is('bekende-adressen') ? 'active' : '') }} {{ (Request::is('bekende-adressen/*') ? 'active' : '') }}">
       <a href="/bekende-adressen">
-          <?php echo file_get_contents("images/Location.svg"); ?> <span class="icon"> Bekende adressen </span>
+          {{--<?php echo file_get_contents("images/Location.svg"); ?> <span class="icon"> Bekende adressen </span>--}}
       </a>
     </li>
     <li class="menu-item {{ (Request::is('exporteren') ? 'active' : '') }}">
       <a href="/exporteren">
-          <?php echo file_get_contents("/images/Export.svg"); ?> <span class="icon"> Exporteren </span>
+          {{--<?php echo file_get_contents("images/Export.svg"); ?> <span class="icon"> Exporteren </span>--}}
       </a>
     </li>
   </ul>
